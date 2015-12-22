20871be0253616df095e08dd52ffb7dafa4701d1
Eric Wang <Eric.B.Wang@alcatel-lucent.com>
2015-12-22 13:34:03 +0800

    operate firewall

--- comoam-web/src/main/java/com/alu/omc/oam/service/COMValidationService.java
+++ comoam-web/src/main/java/com/alu/omc/oam/service/COMValidationService.java
@@ -161,8 +161,8 @@ public class COMValidationService {
     		OutputStream outstream = channel.getOutputStream();
     		InputStream in=channel.getInputStream();
 			outstream.write(finalCommand.getBytes());
-			try{Thread.sleep(3000);}catch(Exception ee){}
 			outstream.flush();
+			try{Thread.sleep(3000);}catch(Exception ee){}
 			System.out.println("The command " + command + " is excuted");
 			byte[] tmp=new byte[2048];
 			while(in.available()>0){
@@ -171,7 +171,7 @@ public class COMValidationService {
 				string = new String(tmp, 0, i);
 				System.out.print(string);
 		    }
-			try{Thread.sleep(1000);}catch(Exception ee){}
+			try{Thread.sleep(2000);}catch(Exception ee){}
             outstream.close();
             in.close();
 		} catch (IOException e) {
