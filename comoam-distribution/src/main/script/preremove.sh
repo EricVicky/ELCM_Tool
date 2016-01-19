91905372be34e464648e1dbdacc8f32d0bbaa954
Jeff Hu <jeff.hu1@alcatel-lucent.com>
2016-01-19 15:08:05 +0800

    force shutdown ELCM if fail to shutdwon

--- comoam-distribution/src/main/script/preremove.sh
+++ comoam-distribution/src/main/script/preremove.sh
@@ -5,6 +5,13 @@ if [ ! -z "$PRO_COMOAM" ]; then
     /opt/PlexView/ELCM/server/bin/shutdown.sh
     sleep 5 
 fi
+#force shutdown ELCM if the ELCM is still runnig
+SERVER_PROCESS_ID=$(ps -ef|grep '/opt/PlexView/ELCM/server'|grep -v 'grep'|awk '{print $2}')
+if [ ! -z $SERVER_PROCESS_ID ]; then
+   echo "force shutdown..."
+   kill -15 $SERVER_PROCESS_ID
+fi
+
 rm -f /opt/PlexView/ELCM/datasource.tar.gz
 rm -f /opt/PlexView/ELCM/cert.tar.gz
 echo "backup data"
