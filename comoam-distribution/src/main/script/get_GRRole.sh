4e749ffb2f511bd7fa217505233a8f191fb97ef3
Eric Wang <Eric.B.Wang@alcatel-lucent.com>
2016-01-25 11:25:11 +0800

    Eric GR Role test

--- comoam-distribution/src/main/script/get_GRRole.sh
+++ comoam-distribution/src/main/script/get_GRRole.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #env|grep scripts
 
-export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/alcatel/omc1/OMC_MON/scripts:/alcatel/omc1/OMC_MON/scripts/tools:/usr/lib/jvm/java/jre/bin:/usr/lib/jvm/java/bin
-
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/alcatel/omc1/OMC_MON/scripts:/alcatel/omc1/OMC_MON/scripts/tools:/usr/lib/jvm/java/jre/bin:/usr/lib/jvm/java/bin
 
 #/alcatel/omc1/OMC_MON/scripts/tools/omc_stat | grep 'THIS SERVER ROLE'
 omc_stat | grep 'THIS SERVER ROLE'
