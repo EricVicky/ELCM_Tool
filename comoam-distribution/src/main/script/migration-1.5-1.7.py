1acdda64b95a58d2bd39e38444f9c58d1a3b4bdb
Jeff Hu <jeff.hu1@alcatel-lucent.com>
2016-02-18 10:02:14 +0800

    replace only active_host_ip and host.ip_address

--- comoam-distribution/src/main/script/migration-1.5-1.7.py
+++ comoam-distribution/src/main/script/migration-1.5-1.7.py
@@ -3,6 +3,7 @@ import json
 import sys
 import socket
 import re
+import traceback
 
 comstackfile="/opt/PlexView/ELCM/datasource/comstack.json"
 hostsfile="/opt/PlexView/ELCM/datasource/hosts.json"
@@ -13,12 +14,17 @@ def getHostIP():
     return hostip
 
 #replace the deployment_preifix, localhost, 127.0.0.1
-def replaceVars(comConfig, deployment_prefix, hostip):
+def replaceVars(comConfigStr, deployment_prefix, hostip):
     p=re.compile("\{\{\s+deployment_prefix\s+\}\}")
-    comConfig = p.sub(deployment_prefix, comConfig)
-    p=re.compile("\"127\.0\.0\.1\"|\"localhost\"")
-    comConfig = p.sub("\"" + hostip + "\"", comConfig)
-    return comConfig
+    comConfigStr = p.sub(deployment_prefix, comConfigStr)
+    comConfig = json.loads(comConfigStr)
+    if comConfig['active_host_ip'] == '127.0.0.1':
+        comConfig['active_host_ip'] = hostip
+    if 'host' in comConfig:
+        if comConfig['host']['ip_address'] == '127.0.0.1':
+            comConfig['host']['ip_address'] = hostip      
+    comConfigStr = json.dumps(comConfig)
+    return comConfigStr
 
 #replace the locahost and 127.0.0.1 as real IP
 def replceHosts(hostip):
@@ -33,6 +39,7 @@ def replceHosts(hostip):
                 json.dump(hosts, hostsfs,  indent=2)
         except Exception:
             print 'failed to load comstacks'
+            traceback.print_exc()
 
 #save the change to comstack.json file
 def writecomconfig(comstacks):
@@ -52,3 +59,4 @@ with open(comstackfile, 'r') as comstacksfs:
         replceHosts(hostIP)
     except Exception:
         print 'failed to load comstacks'
+        traceback.print_exc()
