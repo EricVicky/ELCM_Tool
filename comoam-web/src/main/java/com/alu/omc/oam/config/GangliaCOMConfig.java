5dbdcf3bca5af144644116d5fe7da90dfb0651d2
Peter Pan <peter.pan1@alcatel-lucent.com>
2016-03-02 16:15:06 +0800

    OMC-39588 modify full backup issue

--- comoam-web/src/main/java/com/alu/omc/oam/config/GangliaCOMConfig.java
+++ comoam-web/src/main/java/com/alu/omc/oam/config/GangliaCOMConfig.java
@@ -1,5 +1,12 @@
 package com.alu.omc.oam.config;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GangliaCOMConfig extends OVMCOMConfig {
 
 	@Override
